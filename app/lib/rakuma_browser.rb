4225c38d9e29361a2ba3f9fc88bf207afbce326b
Akira Kano <a.kano1101@gmail.com>
2021-01-10 11:57:11 +0900

    🐛deleteメソッド内.okがうまく動作しないのを改良してみている。調査コードの除去。他。

--- app/lib/rakuma_browser.rb
+++ app/lib/rakuma_browser.rb
@@ -7,9 +7,7 @@ class RakumaBrowser
   
   USER_DATA_DIR = %W[--user-data-dir=./UserData/]
   def self.wake_up
-    client = Selenium::WebDriver::Remote::Http::Default.new
-    client.read_timeout = 2 # seconds – default is 60
-    browser = Watir::Browser.new(:chrome, switches: USER_DATA_DIR, http_client: client) #headless true
+    browser = Watir::Browser.new :chrome, switches: USER_DATA_DIR
     goto_mypage(browser)
     browser
   end
@@ -108,15 +106,7 @@ class RakumaBrowser
     # 「続きを見る」最後まで全展開
     while self.next_button_anchor(browser).exists? # 最後まで「続きを見る」を開くために存在を確認している
       self.next_button_anchor(browser).click # 「続きを見る」をクリック
-      # ページ更新状態の調査ループ
-      loop do
-        is_loaded = had_page_load_completed(browser) # TODO : document.readyStateの仕様を確認したい
-        # 次はもしfalseが出るならクリックで一度ページがcompleteでなくなることを表す（重要確認事項）
-        p 'document.readyStateがcomplete(ページを読み込み終えている)であれば真です。：' + is_loaded.to_s
-        break if is_loaded
-      end
-      # それを待たずしてここへ入らなければならないかもしれないので厄介
-      self.wait_while_next_button_present(browser) # 「続きを見る」が消えるのを待ち次へ（すでに次の「続きを見る」が表示されていたらここでTimeoutを吐く）
+      self.wait_while_next_button_present(browser) # 「続きを見る」が消えるのを待ち次へ（すでに次の「続きを見る」が表示されていたらここでTimeoutを吐くだろう）
     end
   end
 
